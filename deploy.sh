50d51caa3928ce51dca2f77793a316c7e5049769
Lucas Bonnet <lbonnet@bearstech.com>
2020-11-19 10:30:19 +0100

    actually we don't need mongotopy on the delayed secondaries

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # requires SSH agent forwarding
-servers="archi loquy boole sonic late1 late2 higgs"
+servers="archi loquy boole sonic higgs"
 for server in $servers; do
   echo $server
   ssh root@$server.lichess.ovh "cd /home/mongotopy && git pull origin master && systemctl restart mongotopy"
