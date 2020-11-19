f386ccb17790b954faf329a8b3f801c7a8fba19c
Lucas Bonnet <lbonnet@bearstech.com>
2020-11-19 10:26:27 +0100

    update server list

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # requires SSH agent forwarding
-servers="dirac gauss erdos euler"
+servers="archi loquy boole sonic late1 late2 higgs"
 for server in $servers; do
   echo $server
   ssh root@$server.lichess.ovh "cd /home/mongotopy && git pull origin master && systemctl restart mongotopy"
